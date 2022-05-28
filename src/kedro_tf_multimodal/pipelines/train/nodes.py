60292aa8171b87abb47cc73aa2fb05338bf5050b
Bell Eapen <github@gulfdoctor.net>
2022-05-28 09:42:19 -0500

    list ordering

--- src/kedro_tf_multimodal/pipelines/train/nodes.py
+++ src/kedro_tf_multimodal/pipelines/train/nodes.py
@@ -14,20 +14,25 @@ def train_multimodal(text_dataset, image_dataset, multi_modal, parameters):
     """
     Train multimodal model
     """
-    y = text_dataset.pop(parameters['TARGET'])
-    text_dataset.drop(parameters['DROP'], axis=1, inplace=True)
 
-    ids = image_dataset.keys()
-    _imgs = np.array([np.array(image_dataset[id]()) for id in ids]) # column is a function that returns image data
+    _image_dataset = dict(sorted(image_dataset.items())) # sort by key that is the same as parameters['ID']
+    _text_dataset = text_dataset.sort_values(by=[parameters['ID']])
+
+    y = _text_dataset.pop(parameters['TARGET'])
+    _text_dataset.drop(parameters['DROP'], axis=1, inplace=True)
+
+
+    ids = _image_dataset.keys()
+    imgs = np.array([np.array(_image_dataset[id]()) for id in ids]) # column is a function that returns image data
     csv_features_dict = {name: np.array(value)
-                         for name, value in text_dataset.items()}
+                         for name, value in _text_dataset.items()}
 
 
     multi_modal.compile(loss='binary_crossentropy',
                         optimizer=Adam(), metrics=['accuracy'])
 
     hist = multi_modal.fit(
-        x=[csv_features_dict, _imgs],
+        x=[csv_features_dict, imgs],
         y=y, batch_size=32, epochs=3, verbose=1,
         validation_data=None)
     return multi_modal
