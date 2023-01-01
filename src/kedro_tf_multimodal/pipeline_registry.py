79b348b46602b91bb4510788089b1ba7b876219d
Bell Eapen <github@gulfdoctor.net>
2023-01-01 17:50:00 -0500

    fusion in progress

--- src/kedro_tf_multimodal/pipeline_registry.py
+++ src/kedro_tf_multimodal/pipeline_registry.py
@@ -1,19 +1,16 @@
 """Project pipelines."""
 from typing import Dict
 
-from kedro.pipeline import Pipeline, pipeline
-from kedro_tf_utils.pipelines import fusion
-from kedro_tf_multimodal.pipelines.train.pipeline import create_train_pipeline
+from kedro.pipeline import Pipeline
+from kedro_tf_multimodal.pipelines.train.pipeline import create_pipeline
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
 
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    catalog = dict()
 
     return {
-        "__default__": fusion.pipeline.create_fusion_pipeline(catalog) + create_train_pipeline(catalog),
-        "train": create_train_pipeline(catalog)
+        "__default__": create_pipeline(),
 
         }
--- src/kedro_tf_multimodal/pipelines/train/pipeline.py
+++ src/kedro_tf_multimodal/pipelines/train/pipeline.py
@@ -5,12 +5,21 @@ generated using Kedro 0.18.1
 
 from kedro.pipeline import Pipeline, node, pipeline
 
+from kedro_tf_utils.pipelines.train.pipeline import create_train_pipeline
+from kedro_tf_utils.pipelines.fusion.pipeline import create_fusion_pipeline
+from kedro.pipeline.modular_pipeline import pipeline as modular_pipeline
+from kedro_tf_text.pipelines.bert.pipeline import download_bert
 
+
+
+bert_model = modular_pipeline(pipe=download_bert, parameters={"params:bert_model": "params:fusion"})
+
+inputs = {"parameters": "params:fusion", "bert_model": "bert_model_saved",
+          "image_model": "chexnet_weights"}
+_fusion_pipeline = create_fusion_pipeline(**inputs)
+fusion_model = modular_pipeline(pipe=_fusion_pipeline, outputs={
+                                "fusion_model": "fusion_model"})
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([
-        node(
-            
-    ])
-
+    return bert_model + fusion_model
 
