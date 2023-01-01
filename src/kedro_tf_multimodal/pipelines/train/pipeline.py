a9d252d2181a2f9d5b42e316be15900d1fb8d056
Bell Eapen <github@gulfdoctor.net>
2023-01-01 18:05:02 -0500

    fusion complete

--- src/kedro_tf_multimodal/pipelines/train/pipeline.py
+++ src/kedro_tf_multimodal/pipelines/train/pipeline.py
@@ -10,16 +10,20 @@ from kedro_tf_utils.pipelines.fusion.pipeline import create_fusion_pipeline
 from kedro.pipeline.modular_pipeline import pipeline as modular_pipeline
 from kedro_tf_text.pipelines.bert.pipeline import download_bert
 
+from kedro_tf_text.pipelines.tabular.pipeline import tabular_model_pipeline
 
 
 bert_model = modular_pipeline(pipe=download_bert, parameters={"params:bert_model": "params:fusion"})
+tabular_model = modular_pipeline(pipe=tabular_model_pipeline, outputs={"tabular_model": "tabular_model_saved"}, parameters={
+                              "params:tabular": "params:fusion"})
 
-inputs = {"parameters": "params:fusion", "bert_model": "bert_model_saved",
+
+inputs = {"parameters": "params:fusion", "bert_model": "bert_model_saved", "tabular_model": "tabular_model_saved",
           "image_model": "chexnet_weights"}
 _fusion_pipeline = create_fusion_pipeline(**inputs)
 fusion_model = modular_pipeline(pipe=_fusion_pipeline, outputs={
                                 "fusion_model": "fusion_model"})
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return bert_model + fusion_model
+    return bert_model + tabular_model + fusion_model
 
