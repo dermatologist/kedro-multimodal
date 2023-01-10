fc93b5e8720dc9d0dd0119f1ef3d82b335af9e5d
Bell Eapen <github_public@gulfdoctor.net>
2023-01-10 21:25:43 +0000

    Update serving

--- serving.py
+++ serving.py
@@ -10,6 +10,7 @@ def main():
 
     # Load an image from file
     im = cv2.imread("data/01_raw/imageset/img1.jpg")
+    im2 = cv2.imread("data/01_raw/imageset/img3.jpg")
 
     # Base64 encode it
     # (you can also load the raw png data from file using open instead of opencv)
@@ -30,16 +31,15 @@ def main():
     _json_data = {
         "signature_name": "serving_default",
         "instances": [{
-            "age": 24,
-            "bp": 85,
-            "gender": "Male",
+            "age": 64,
+            "bp": 121,
+            "gender": "Female",
             "zip": 12345,
             "text_input_for_bert": "This is a test sentence.",
-            "input_1": np.asarray(im).tolist()
+            "input_1": np.asarray(im2).tolist()
         }]
     }
 
-    print(_json_data)
 
     # Wrap it in json (tf-serving compatible with instances and b64)
     data = json.dumps(json_data)
@@ -49,8 +49,18 @@ def main():
     resp = requests.post("http://localhost:8501/v1/models/train:predict", data)
 
     # Load base 64 encoded image string from response
-    print(resp.json())
+    print("Response with b64: {}".format(resp.json()))
 
+
+    # Wrap it in json (tf-serving compatible with instances and b64)
+    data = json.dumps(_json_data)
+
+    # Standard port for tf-serving rest interface is 8501
+    # Request Format: http://domain:[tf-serving-port]/v1/models/[model_name]:predict
+    resp = requests.post("http://localhost:8501/v1/models/train:predict", data)
+
+    # Load base 64 encoded image string from response
+    print("Response with Array: {}".format(resp.json()))
 
 if __name__ == "__main__":
   main()
